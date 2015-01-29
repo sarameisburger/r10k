3061089e9aaa451066853172734760b1004e978b
Michael Stahnke <stahnma@puppetlabs.com>
2015-01-29 19:09:21 +0000

    Tagging for a build. This is the first package I'll hand to Adrien.

--- configs/components/rubygem-rugged.rb
+++ configs/components/rubygem-rugged.rb
@@ -16,6 +16,7 @@ component "rubygem-rugged" do |pkg, settings, platform|
   end
 
   pkg.install do
-    "PKG_CONFIG_PATH=#{settings[:pkg_config_path]} #{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem -- --with-opt-dir=/opt/puppetlabs/agent/  "
+    #[ "PKG_CONFIG_PATH=#{settings[:pkg_config_path]} #{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem -- --with-opt-dir=/opt/puppetlabs/agent/  ", "rm -rf /opt/puppetlabs/server/apps/code-management/gems/rugged-0.21.4/vendor/libgit2/build/" , "rm -rf /opt/puppetlabs/server/apps/code-management/gems/rugged-0.21.4/vendor/libgit2/src /opt/puppetlabs/server/apps/code-management/gems/rugged-0.21.4/vendor/libgit2/include /opt/puppetlabs/server/apps/code-management/gems/rugged-0.21.4/vendor/libgit2/deps /opt/puppetlabs/server/apps/code-management/gems/rugged-0.21.4/vendor/libgit2/cmake" ]
+    [ "PKG_CONFIG_PATH=#{settings[:pkg_config_path]} #{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem -- --with-opt-dir=/opt/puppetlabs/agent/  " ]
   end
 end
