e06e127c0d9e549e7badbee7fe534fbb60e8b27a
Michael Stahnke <stahnma@puppetlabs.com>
2015-02-02 18:31:50 +0000

    Add buildtools repo properly to el platform definitions

--- configs/platforms/el-6-x86_64.rb
+++ configs/platforms/el-6-x86_64.rb
@@ -4,7 +4,9 @@ platform "el-6-x86_64" do |plat|
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "sysv"
 
-  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign;echo '[pl-build-tools]\nname=pl-build-tools\ngpgcheck=0\nbaseurl=http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/$basearch' > /etc/yum.repos.d/pl-build-tools.repo"
+  plat.yum_repo "http://builds.puppetlabs.lan/puppet-agent/#{tag}/repo_configs/rpm/pl-puppet-agent-#{tag}-#{plat.get_name}.repo"
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-el-6.noarch.rpm"
+  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.vcloud_name "centos-6-x86_64"
 end
--- configs/platforms/el-7-x86_64.rb
+++ configs/platforms/el-7-x86_64.rb
@@ -3,8 +3,9 @@ platform "el-7-x86_64" do |plat|
   plat.servicedir "/usr/lib/systemd/system"
   plat.defaultdir "/etc/sysconfig"
   plat.servicetype "systemd"
-  plat.yum_repo "http://builds.puppetlabs.lan/puppet-agent/#{tag}/repo_configs/rpm/pl-puppet-agent-#{tag}-el-7-x86_64.repo"
-  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign;echo '[pl-build-tools]\nname=pl-build-tools\ngpgcheck=0\nbaseurl=http://pl-build-tools.delivery.puppetlabs.net/yum/el/6/$basearch' > /etc/yum.repos.d/pl-build-tools.repo "
+  plat.yum_repo "http://builds.puppetlabs.lan/puppet-agent/#{tag}/repo_configs/rpm/pl-puppet-agent-#{tag}-#{plat.get_name}.repo"
+  plat.yum_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/pl-build-tools-release-el-7.noarch.rpm"
+  plat.provision_with "yum install --assumeyes autoconf automake createrepo rsync gcc make rpmdevtools rpm-libs yum-utils rpm-sign"
   plat.install_build_dependencies_with "yum install --assumeyes"
   plat.vcloud_name "centos-7-x86_64"
 end
