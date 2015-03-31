7da4317947e48af438f243405309e9d67d2ac046
Adrien Thebo <git@somethingsinistral.net>
2015-03-31 16:30:24 -0700

    bump multi_json to 1.11.0

--- configs/components/rubygem-multi_json.rb
+++ configs/components/rubygem-multi_json.rb
@@ -1,9 +1,9 @@
 component "rubygem-multi_json" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
-  pkg.version "1.8.4"
-  pkg.md5sum "5040903eb3f0c338c4d6b46fdc66ea7a"
-  puts  "checking http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
-  pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
+
+  pkg.version "1.11.0"
+  pkg.md5sum "d9b22b25fd249e2b075cbac2ece6625b"
+  pkg.url "http://media.somethingsinistral.net/gems/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.build_requires "pe-ruby"
 
