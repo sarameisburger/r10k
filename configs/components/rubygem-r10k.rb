66ccd5e29ebd0ae841868528cb465094f1a39413
Adrien Thebo <git@somethingsinistral.net>
2015-03-31 10:24:17 -0700

    build r10k from git using r10k.json

--- configs/components/rubygem-r10k.rb
+++ configs/components/rubygem-r10k.rb
@@ -1,17 +1,16 @@
 component "rubygem-r10k" do |pkg, settings, platform|
-  gemname = pkg.get_name.gsub('rubygem-', '')
-  pkg.version "1.4.1"
-  pkg.md5sum "6b7ae57f59c593fa42833b9cf0798bc3"
-  puts  "checking http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
-  pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
+
+  pkg.load_from_json('configs/components/rubygem-r10k.json')
+  pkg.version '1.4.2'
 
   pkg.build_requires "pe-ruby"
+  pkg.build_requires "git"
 
   pkg.install do
-    [ "#{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem"  ,
+    [
+      "gem build r10k.gemspec",
+      "#{settings[:gem_inst]} r10k-#{pkg.get_version}.gem"  ,
       %q{sed -i '/require./aENV["GEM_HOME"]="\/opt\/puppet\/lib\/r10k"\nGem.path.unshift("\/opt\/puppet\/lib\/r10k")\nGem.refresh'  /opt/puppet/bin/r10k},
     ]
   end
-
-  pkg.install_file("#{settings[:libdir]}/r10k/gems/r10k-#{pkg.get_version}/r10k.yaml.example", "#{settings[:sysconfdir]}/r10k.yaml.example")
 end
