95af547581a073ca801ed7a66bcfebc46ade1009
Rob Braden <bradejr@puppetlabs.com>
2015-05-29 09:56:59 -0700

    (re-4636) Add sles and ubuntu precise to build_defaults

---
+++ configs/platforms/sles-12-x86_64.rb
@@ -0,0 +1,11 @@
+peversion = IO.read('peversion').strip
+platform "sles-12-x86_64" do |plat|
+  plat.servicedir "/etc/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
+  plat.zypper_repo "http://enterprise.delivery.puppetlabs.net/#{peversion}/repos/#{plat.get_name}/#{plat.get_name}.repo"
+  plat.zypper_repo "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/12/x86_64/pl-build-tools-sles-12-x86_64.repo"
+  plat.provision_with "zypper -n --no-gpg-checks install -y aaa_base autoconf automake rsync gcc make rpm-build"
+  plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
+  plat.vcloud_name "sles-12-x86_64"
+end
