8aa6300c152518eac0353f2a28c37ca25ad6e164
Jesse Scott <jesse@puppetlabs.com>
2016-05-17 10:13:28 -0700

    (CODEMGMT-708) (CODEMGMT-709) Bump r10k, puppet_forge and add typhoeus/ethon/ffi.

---
+++ configs/components/rubygem-ffi.rb
@@ -0,0 +1,26 @@
+component "rubygem-ffi" do |pkg, settings, platform|
+  gemname = pkg.get_name.gsub('rubygem-', '')
+  pkg.version "1.9.10"
+  pkg.md5sum "dede6f5db06f699153b5cdf24c0e7b08"
+  pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
+
+  pkg.build_requires "puppet-agent"
+  pkg.build_requires "cmake"
+  if platform.is_rpm?
+    # red hat
+    pkg.build_requires "pkgconfig"
+  else
+    # debian
+    pkg.build_requires "pkg-config"
+  end
+
+  pkg.install do
+    [
+      "PKG_CONFIG_PATH='#{settings[:pkg_config_path]}' \
+      CFLAGS='#{settings[:cflags]}' \
+      LDFLAGS='#{settings[:ldflags]}' \
+      CC=/opt/pl-build-tools/bin/gcc \
+      #{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem -- --with-opt-dir=#{settings[:prefix]} --use-system-libraries"
+    ]
+  end
+end
--- configs/components/rubygem-typhoeus.rb
+++ configs/components/rubygem-typhoeus.rb
@@ -1,10 +1,8 @@
-component "rubygem-puppet_forge" do |pkg, settings, platform|
+component "rubygem-typhoeus" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
-  pkg.version "2.2.0"
-  pkg.md5sum "b4ff4094328617e46ee939641083ba04"
+  pkg.version "1.0.2"
+  pkg.md5sum "c5cfed3f715bf8788209bdce9b240782"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
-
-  pkg.build_requires "puppet-agent"
 
   pkg.install do
     "#{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem"
