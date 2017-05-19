f044cb8490d67b19580b4304a5f853d189d9f32e
Andrew Roetker <andrew.roetker@puppetlabs.com>
2017-05-19 15:04:01 -0700

    (CODEMGMT-1064) Use the correct md5sum for the cri 2.8.0 gem
    This commit fixes the md5sum for the cri 2.8.0 gem because the old
    md5sum was for a corrupt/incomplete version of the gem.

--- configs/components/rubygem-cri.rb
+++ configs/components/rubygem-cri.rb
@@ -1,7 +1,7 @@
 component "rubygem-cri" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
   pkg.version "2.8.0"
-  pkg.md5sum "0b2608ce3ba55bbbecb249066e6f4c0c"
+  pkg.md5sum "3c9e6562617307080b42527f67ee25a7"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.build_requires "puppet-agent"
