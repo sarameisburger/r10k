084160c86c37d089f353f859b19291eea9daebbe
Anderson Mills <anderson@puppet.com>
2017-07-19 13:27:51 -0500

    (PE-21536) Update cri to build r10k v2.5.5.

--- configs/components/rubygem-cri.rb
+++ configs/components/rubygem-cri.rb
@@ -1,7 +1,7 @@
 component "rubygem-cri" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
-  pkg.version "2.8.0"
-  pkg.md5sum "3c9e6562617307080b42527f67ee25a7"
+  pkg.version "2.6.1"
+  pkg.md5sum "657c231f411fc9b7f630dcb980517bce"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.build_requires "puppet-agent"
