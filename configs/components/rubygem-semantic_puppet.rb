2bc39748d8c896194c7547c167d35bb9258c81f9
Anderson Mills <anderson@puppet.com>
2017-10-23 16:37:28 -0500

    (RK-301) Use correct md5s for dependencies.

--- configs/components/rubygem-semantic_puppet.rb
+++ configs/components/rubygem-semantic_puppet.rb
@@ -1,7 +1,7 @@
 component "rubygem-semantic_puppet" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
   pkg.version "1.0.1"
-  pkg.md5sum "3188115df61e38afc102144848a13a8e"
+  pkg.md5sum "e6d0c311cef68f799019b8a41e0fa8b8"
   pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.build_requires "puppet-agent"
