09160942bd4ba83bb13cd8bf9c33d5eefd0b516c
Anderson Mills <anderson@puppet.com>
2017-10-25 13:30:49 -0500

    (PE-22665) Re-add semantic_puppet to pe-r10k.

---
+++ configs/components/rubygem-semantic_puppet.rb
@@ -0,0 +1,13 @@
+component "rubygem-semantic_puppet" do |pkg, settings, platform|
+  gemname = pkg.get_name.gsub('rubygem-', '')
+  pkg.version "1.0.1"
+  pkg.md5sum "8ce85a0206a640654e0019eadf615672"
+  pkg.url "https://rubygems.org/downloads/#{gemname}-#{pkg.get_version}.gem"
+  pkg.mirror "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
+
+  pkg.build_requires "puppet-agent"
+
+  pkg.install do
+    "#{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem"
+  end
+end
