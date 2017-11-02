d92eaecdb0e9c14489e30b516251c5e2af80fdf4
Molly Waggett <molly.waggett@puppet.com>
2017-11-02 10:40:17 -0700

    (RE-9648) Migrate vanagon projects use buildsources in Artifactory
    This commit adds `artifactory_url` and `buildsources_url` settings and replaces
    buildsources.delivery.puppetlabs.net with Artifactory.

--- configs/components/rubygem-ethon.rb
+++ configs/components/rubygem-ethon.rb
@@ -2,7 +2,7 @@ component "rubygem-ethon" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
   pkg.version "0.9.0"
   pkg.md5sum "284755a26e0d60e7bcfd684ece0630b6"
-  pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
+  pkg.url "#{settings[:buildsources_url]}/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.install do
     "#{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem"
--- configs/components/rubygem-semantic_puppet.rb
+++ configs/components/rubygem-semantic_puppet.rb
@@ -2,7 +2,7 @@ component "rubygem-semantic_puppet" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
   pkg.version "1.0.1"
   pkg.md5sum "8ce85a0206a640654e0019eadf615672"
-  pkg.url "http://buildsources.delivery.puppetlabs.net/#{gemname}-#{pkg.get_version}.gem"
+  pkg.url "#{settings[:buildsources_url]}/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.build_requires "puppet-agent"
 
