6bc87e4ebebc70620cb214a4e4743f4d5ce16368
Maggie Dreyer <maggie@puppet.com>
2018-07-18 10:43:03 -0700

    (PE-24637) Add platform config for Ubuntu 18.04
    This commit adds a platform config for Ubuntu 18.04 Bionic.

--- configs/components/rubygem-rugged.rb
+++ configs/components/rubygem-rugged.rb
@@ -14,12 +14,18 @@ component "rubygem-rugged" do |pkg, settings, platform|
     pkg.build_requires "pkg-config"
   end
 
+  if platform.name =~ /ubuntu-18/
+    compiler_path = "/usr/bin/gcc"
+  else
+    compiler_path = "/opt/pl-build-tools/bin/gcc"
+  end
+
   pkg.install do
     [
       "PKG_CONFIG_PATH='#{settings[:pkg_config_path]}' \
       CFLAGS='#{settings[:cflags]}' \
       LDFLAGS='#{settings[:ldflags]}' \
-      CC=/opt/pl-build-tools/bin/gcc \
+      CC=#{compiler_path} \
       #{settings[:gem_inst]} #{gemname}-#{pkg.get_version}.gem -- --with-opt-dir=#{settings[:prefix]} --use-system-libraries"
     ]
   end
