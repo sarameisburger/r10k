a2c379936b503ccb03d6ec6dfaaf1ffcb3340be1
Maggie Dreyer <maggie@puppet.com>
2018-07-27 10:24:38 -0700

    (RK-321) Update cri to 2.8.0
    This brings the cri gem up to date with the version required by
    r10k#master.

--- configs/components/rubygem-cri.rb
+++ configs/components/rubygem-cri.rb
@@ -1,7 +1,7 @@
 component "rubygem-cri" do |pkg, settings, platform|
   gemname = pkg.get_name.gsub('rubygem-', '')
-  pkg.version "2.6.1"
-  pkg.md5sum "657c231f411fc9b7f630dcb980517bce"
+  pkg.version "2.8.0"
+  pkg.md5sum "3c9e6562617307080b42527f67ee25a7"
   pkg.url "#{settings[:buildsources_url]}/#{gemname}-#{pkg.get_version}.gem"
 
   pkg.build_requires "puppet-agent"
